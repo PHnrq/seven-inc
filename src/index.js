339f5d15fe9c952a2579eb044c702d66008e5bec
PHnrq <paulofigueredo7@gmail.com>
2022-09-02 09:51:13 -0300

    Configure miraje js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createServer, Model } from "miragejs";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+  createServer({
+    models: {
+      employee: Model,
+    },
+
+    seeds(server) {
+      server.create("employee",        {
+        id: 1,
+        name: "José Ferreira",
+        document: "111.111.111-00",
+        email: "jose@email.com",
+        phone: "(11)9.8765-4321",
+        birth_date: "21/01/2000",
+        salary: "1000",
+        create_at: new Date()
+      })
+    },
+
+    routes() {
+      this.get("/api/employees", (schema) => {
+        return schema.employees.all()
+      })
+
+      this.post("/api/employees", (schema, request) => {
+        let attrs = JSON.parse(request.requestBody)
+
+        return schema.employees.create(attrs)
+      })
+    },
+  })
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
