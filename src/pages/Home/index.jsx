72eacca8a769ff3290d94d633a4399d4ed8235cd
PHnrq <paulofigueredo7@gmail.com>
2022-09-02 09:52:10 -0300

    Add Home Page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,35 +1,99 @@
-import { Grid, Stack, Box } from "@mui/material";
-import EmployeeCard from "./EmployeeCard";
-import Header from "./Header";
+import { useState, useEffect } from "react";
 
-export function Home(){
+import Header from "../../components/Header";
+import Box from "@mui/material/Box";
+
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+import { DataGrid } from "@mui/x-data-grid";
+import EmployeeDetails from "../../components/EmployeeDetails";
+import { FormSingUp } from "../../components/FormSingUp";
+import { api } from "../../services/api";
+
+const columns = [
+  {
+    field: "name",
+    headerName: "Nome",
+    width: 250,
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    width: 250,
+  },
+  {
+    field: "phone",
+    headerName: "Telefone",
+    width: 150,
+  },
+  {
+    field: "salary",
+    headerName: "Salário",
+    width: 130,
+  },
+  {
+    field: "created_at",
+    headerName: "Data da contratação",
+    width: 200,
+  },
+];
+
+export function Home() {
+  const [employees, setEmployees] = useState([]);
+
+  const [openEmployeesDetail, setOpenEmployeesDetail] = useState(false);
+  const handleOpenEmployeesDetail = () => setOpenEmployeesDetail(true);
+  const handleCloseEmployeesDetail = () => setOpenEmployeesDetail(false);
+
+  const [openSingUp, setOpenSingUp] = useState(false);
+  const handleOpenSingUp = () => setOpenSingUp(true);
+  const handleCloseSingUp = () => setOpenSingUp(false);
+
+  const [showDetails, setShowDetails] = useState(false);
+  const [selectedEmployee, setSelectedEmployee] = useState({});
+
+  function handleShowDetails(e) {
+    const employee = employees[e.id - 1];
+    setSelectedEmployee(employee);
+    handleOpenEmployeesDetail();
+  }
+
+  useEffect(() => {
+    api.get("employees").then((response) => {
+      console.log(response.data.employees);
+      setEmployees(response.data.employees);
+    });
+  }, []);
+
   return (
-    <Box sx={{ width: '100%'}}>
+    <>
       <Header />
-      <Box sx={{ flexGrow: 1, width: '100%',maxWidth: 1200, mx: "auto", py: 6}}>
-        <Grid  container spacing={6}>
-          <Grid item xs={4}>
-            <EmployeeCard />
-          </Grid>
-          <Grid item xs={4}>
-            <EmployeeCard />
-          </Grid>
-          <Grid item xs={4}>
-            <EmployeeCard />
-          </Grid>
+      <EmployeeDetails
+        employee={selectedEmployee}
+        handleClose={handleCloseEmployeesDetail}
+        open={openEmployeesDetail}
+      />
 
-          <Grid item xs={4}>
-            <EmployeeCard />
-          </Grid>
-          <Grid item xs={4}>
-            <EmployeeCard />
-          </Grid>
-          <Grid item xs={4}>
-            <EmployeeCard />
-          </Grid>
-        </Grid>
+      <Box sx={{ height: 455, width: "100%", p: 2, cursor: "pointer", display: "flex", flexDirection: 'column'}}>
+        <Button 
+          variant="contained" 
+          endIcon={<AddIcon />} 
+          onClick={handleOpenSingUp} 
+          sx={{maxWidth: 320, alignSelf: 'flex-end'}}
+          >
+          Novo funcionário
+        </Button>
+        <DataGrid
+          rows={employees}
+          columns={columns}
+          pageSize={6}
+          rowsPerPageOptions={[6]}
+          onCellClick={(e) => handleShowDetails(e)}
+          sx={{mt: 2,}}
+        />
+
+        <FormSingUp handleClose={handleCloseSingUp} open={openSingUp}/>
       </Box>
-    </Box>
-
-  )
+    </>
+  );
 }
