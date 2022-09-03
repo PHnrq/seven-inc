15d7cb0915b3538d216b09c0bc24ee2eaf7ae093
PHnrq <paulofigueredo7@gmail.com>
2022-09-03 09:48:32 -0300

    Add columns data template

---
+++ src/utils/columnsDataGridConfig.js
@@ -0,0 +1,27 @@
+export const columns = [
+  {
+    field: "name",
+    headerName: "Nome",
+    width: 250,
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    width: 250,
+  },
+  {
+    field: "phone",
+    headerName: "Telefone",
+    width: 150,
+  },
+  {
+    field: "salary",
+    headerName: "Salário",
+    width: 130,
+  },
+  {
+    field: "created_at",
+    headerName: "Data da contratação",
+    width: 200,
+  },
+];
